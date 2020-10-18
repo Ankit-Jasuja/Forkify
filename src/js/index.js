179f3ba5d25e9644c10bd2b76d87441a06e290d8
Ankit Jasuja <ankitjasuja84@gmail.com>
2020-10-18 20:51:26 +0530

    use axios package to fetch data from api

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,13 @@
 // Global app controller
+import axios from 'axios';
 
-//import num from './test';
-//console.log(`imported value is ${num}`);
-const a = 10;
-console.log(`value of a is ${a}`);
+async function getResults(query){
+    try{
+        const result = await axios(`https://forkify-api.herokuapp.com/api/search?q=${query}`);
+        console.log(result.data.recipes);
+    }
+    catch(error){
+        alert(error);
+    }
+}
+getResults('Pizza');
--- src/js/test.js
+++
@@ -1,4 +0,0 @@
-// console.log('imported module');
- //export default 25;
-
-console.log('ttet');
