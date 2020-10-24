1b079a4f8f13ad49de0997e15e1356d74a463fb6
Ankit Jasuja <ankitjasuja84@gmail.com>
2020-10-24 16:52:53 +0530

    Building Likes model

---
+++ src/js/models/Likes.js
@@ -0,0 +1,32 @@
+import uniqid from 'uniqid';
+
+export default class Likes {
+    constructor(){
+        this.likes = [];
+    }
+     
+    addLike(id,title,author,img){
+     const like = {
+         id:id,
+         title:title,
+         author:author,
+         img:img
+     }
+     this.likes.push(like);
+     return like; //returning in this case is just good practice
+    }
+
+    deleteLike(id){
+      const index = this.likes.findIndex(el=>el.id===id);
+      this.likes.splice(index,1);
+    }
+
+    isLiked(id){
+        return this.likes.findIndex(el=>el.id===id) !== -1;
+    }
+
+    getNumerOfLiked(){
+        return this.likes.length();
+    }
+
+}
